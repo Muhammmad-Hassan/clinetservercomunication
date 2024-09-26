9b94b45988280e8db19d27445f5a990dcdb64a10
Muhammmad-Hassan <hassankhan5uy99@gmail.com>
2024-09-26 07:17:09 -0700

    end point for geting data form server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,4 +5,11 @@ function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [editingItemId, setEditingItemId] = useState(null);
+  const [editingItemId, setEditingItemId] = useState(null);  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const res = await axios.get("http://localhost:5000/api/items");
+    setItems(res.data);
+  };
